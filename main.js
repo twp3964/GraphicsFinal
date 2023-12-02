49fc68aa12c6fe48d95b1c0b782792761cdca5fc
twp3964 <twp3964@g.rit.edu>
2023-12-01 22:29:59 -0500

    Set such that the html has shaders and main a square

--- main.js
+++ main.js
@@ -1,128 +1,161 @@
-'use strict';
+let gl; // Define the global variable to hold the WebGL rendering context
+let program; // Variable to hold the shader program
 
-// Global variables that are set and used
-// across the application
-let gl,
-  program,
-  points,
-  bary,
-  indices;
+function start() {
+  const canvas = document.getElementById("webgl-canvas");
+  gl = canvas.getContext("webgl2");
 
-// VAO stuff
-var myVAO = null;
-var myVertexBuffer = null;
-var myBaryBuffer = null;
-var myIndexBuffer = null;
-
-// Given an id, extract the content's of a shader script
-// from the DOM and return the compiled shader
-function getShader(id) {
-  const script = document.getElementById(id);
-  const shaderString = script.text.trim();
-
-  // Assign shader depending on the type of shader
-  let shader;
-  if (script.type === 'x-shader/x-vertex') {
-    shader = gl.createShader(gl.VERTEX_SHADER);
-  }
-  else if (script.type === 'x-shader/x-fragment') {
-    shader = gl.createShader(gl.FRAGMENT_SHADER);
-  }
-  else {
-    return null;
+  if (!gl) {
+    alert("WebGL isn't available");
+    return;
   }
 
-  // Compile the shader using the supplied shader code
-  gl.shaderSource(shader, shaderString);
-  gl.compileShader(shader);
+  // Vertex shader source code
+  const vsSource = document.getElementById("vertex-shader").textContent;
 
-  // Ensure the shader is valid
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(shader));
-    return null;
-  }
+  // Fragment shader source code
+  const fsSource = document.getElementById("fragment-shader").textContent;
 
-  return shader;
+  // Initialize shaders and program
+  initProgram(vsSource, fsSource);
+
+  // Cube vertices and indices (Replace these with your cube data)
+  const vertices = [
+    // Front face
+    -0.5, -0.5,  0.5,
+     0.5, -0.5,  0.5,
+     0.5,  0.5,  0.5,
+    -0.5,  0.5,  0.5,
+  
+    // Back face
+    -0.5, -0.5, -0.5,
+     0.5, -0.5, -0.5,
+     0.5,  0.5, -0.5,
+    -0.5,  0.5, -0.5,
+  
+    // Top face
+     0.5,  0.5,  0.5,
+    -0.5,  0.5,  0.5,
+    -0.5,  0.5, -0.5,
+     0.5,  0.5, -0.5,
+  
+    // Bottom face
+    -0.5, -0.5,  0.5,
+     0.5, -0.5,  0.5,
+     0.5, -0.5, -0.5,
+    -0.5, -0.5, -0.5,
+  
+    // Right face
+     0.5, -0.5,  0.5,
+     0.5,  0.5,  0.5,
+     0.5,  0.5, -0.5,
+     0.5, -0.5, -0.5,
+  
+    // Left face
+    -0.5, -0.5,  0.5,
+    -0.5,  0.5,  0.5,
+    -0.5,  0.5, -0.5,
+    -0.5, -0.5, -0.5,
+  ];
+  
+
+  const indices = [
+    // Front face
+    0, 1, 2,
+    2, 3, 0,
+  
+    // Back face
+    4, 5, 6,
+    6, 7, 4,
+  
+    // Top face
+    8, 9, 10,
+    10, 11, 8,
+  
+    // Bottom face
+    12, 13, 14,
+    14, 15, 12,
+  
+    // Right face
+    16, 17, 18,
+    18, 19, 16,
+  
+    // Left face
+    20, 21, 22,
+    22, 23, 20,
+  ];
+  
+
+  // Setup buffers and draw the cube
+  setupBuffersAndDraw(vertices, indices);
 }
 
-// Create a program with the appropriate vertex and fragment shaders
-function initProgram() {
-  const vertexShader = getShader('vertex-shader');
-  const fragmentShader = getShader('fragment-shader');
+function initProgram(vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-  // Create a program
   program = gl.createProgram();
-  // Attach the shaders to this program
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
 
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     console.error('Could not initialize shaders');
-  }
-
-  // Use this program instance
-  gl.useProgram(program);
-  // We attach the location of these shader values to the program instance
-  // for easy access later in the code
-  program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
-  program.aBary = gl.getAttribLocation(program, 'bary');
-  program.uTheta = gl.getUniformLocation (program, 'theta');
-}
-
-
-// We call draw to render to our canvas
-function draw() {
-  // Clear the scene
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Bind the VAO
-  gl.bindVertexArray(myVAO);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
-
-  // Draw to the scene using triangle primitives
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-
-  // Clean
-  gl.bindVertexArray(null);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-}
-
-// Entry point to our application
-function init() {
-  // Retrieve the canvas
-  const canvas = document.getElementById('webgl-canvas');
-  if (!canvas) {
-    console.error(`There is no canvas with id ${'webgl-canvas'} on this page.`);
     return null;
   }
 
-  // deal with keypress
-  window.addEventListener('keydown', gotKey ,false);
+  gl.useProgram(program);
 
-  // Retrieve a WebGL context
-  gl = canvas.getContext('webgl2');
-  // Set the clear color to be black
-  gl.clearColor(0, 0, 0, 1);
-    
-  // some GL initialization
-  gl.enable(gl.DEPTH_TEST);
-  gl.enable(gl.CULL_FACE);
-  
-  gl.cullFace(gl.BACK);
-  gl.frontFace(gl.CCW);
-  gl.clearColor(0.0,0.0,0.0,1.0)
-  gl.depthFunc(gl.LEQUAL)
-  gl.clearDepth(1.0)
-
-  // Read, compile, and link your shaders
-  initProgram();
-  
-  // create and bind your current object
-  createNewShape();
-  
-  // do a draw
-  draw();
+  // Specify the attribute location for 'aVertexPosition'
+  program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
+  if (program.aVertexPosition === -1) {
+    console.error('Failed to get the storage location of aVertexPosition');
+    return null;
+  }
 }
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function setupBuffersAndDraw(vertices, indices) {
+  // Create buffer for vertices
+  const vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  // Set up attribute pointers for vertex position
+  const vertexPositionAttribLocation = program.aVertexPosition; // Attribute location for vertex position
+  gl.vertexAttribPointer(
+    vertexPositionAttribLocation,
+    3,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
+  gl.enableVertexAttribArray(vertexPositionAttribLocation);
+
+  // Create buffer for indices
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+  // Draw the cube
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+}
+
+
+window.onload = start;
