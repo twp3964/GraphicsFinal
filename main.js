341bb25b29bdf669ee51d35977e7c30e1771c3ec
twp3964 <twp3964@g.rit.edu>
2023-12-17 17:50:19 -0500

    Made trees but they do look weird.

--- main.js
+++ main.js
@@ -51,35 +51,86 @@ let perlin = {
 }
 perlin.seed();
 
+function drawTree(x, y, z) {
+  let treeHeight = 100; // Set the height of the tree
+  let baseRadius = 10; // Set the base radius of the tree trunk
+
+  push();
+  translate(x, y - treeHeight / 2, z); // Adjust the starting position of the tree
+
+  // Create the tree trunk using Perlin noise
+  beginShape();
+  for (let i = 0; i < treeHeight; i += 5) {
+    let noiseValue = perlin.get(i * 0.1, 0); // Use Perlin noise for variation
+    let radius = map(noiseValue, 0, 1, baseRadius / 2, baseRadius); // Vary the radius based on Perlin noise
+    let angle = map(i, 0, treeHeight, 0, TWO_PI);
+
+    let xOffset = cos(angle) * radius;
+    let zOffset = sin(angle) * radius;
+    vertex(xOffset, i - treeHeight / 2, zOffset); // Adjust the height of the trunk
+  }
+  endShape();
+
+  // Draw leaves on top of the trunk
+  noStroke();
+  fill(34, 139, 34); // Set the color for the leaves
+
+  let leafLength = 30; // Set the length of the leaves
+  let leafWidth = 8; // Set the width of the leaves
+  let leavesCount = 8; // Set the number of leaves
+
+  // Move to the top of the trunk to draw the leaves
+  translate(0, -treeHeight / 2, 0);
+
+  for (let i = 0; i < leavesCount; i++) {
+    let angle = map(i, 0, leavesCount, 0, TWO_PI);
+    let xOffset = cos(angle) * baseRadius * 1.5;
+    let zOffset = sin(angle) * baseRadius * 1.5;
+
+    push();
+    translate(xOffset, 0, zOffset);
+    rotateY(angle + HALF_PI); // Orient the leaves outward
+
+    // Draw leaves as ellipsoids or any other desired shape
+    ellipsoid(leafLength, leafWidth, leafWidth * 0.5);
+    pop();
+  }
+  pop();
+}
+
 
 function setup() {
   createCanvas(400, 400, WEBGL);
   background(220);
-  camera(camX, camY, camZ, 
-        camLookX, 0, camLookZ, 0, 5, 0);
+  camera(camX, camY, camZ, camLookX, 0, camLookZ, 0, 5, 0);
 
   let xoff = 0.0;
   let yoff = 0.0;
   let scl_off = 0.25;
+  let counter = 0;
+
   for (let x = -300; x < 300; x += 20) {
     for (let z = -zRoad; z < zRoad; z += 20) {
       push();
       translate(x, 0, z);
 
-      if (x < 60 && x > -60){
-        box(20,0,20);
-      }else if (x < 80 && x > -80){
-        box(20,map(perlin.get(xoff,yoff),0,1,0,20),20);
-      }else if (x < 100 && x > -100){
-        box(20,map(perlin.get(xoff,yoff),0,1,0,70),20);
-      }else if (x < 100 && x > -100){
-        box(20,map(perlin.get(xoff,yoff),0,1,0,100),20);
-      }else{
-        box(20,map(perlin.get(xoff,yoff),0,1,0,200),20);
-
-        // box(20,random(30,60),20);
+      if (x < 60 && x > -60) {
+        box(20, 20, 20);
+        if ((x == 20 || x == -20) && counter > 2){
+          drawTree(x, 10, z);
+          counter = 0;
+        }
+      } else if (x < 80 && x > -80) {
+        box(20, map(perlin.get(xoff, yoff), 0, 1, 0, 20), 20);
+      } else if (x < 100 && x > -100) {
+        box(20, map(perlin.get(xoff, yoff), 0, 1, 0, 70), 20);
+      } else if (x < 120 && x > -120) {
+        box(20, map(perlin.get(xoff, yoff), 0, 1, 0, 100), 20);
+      } else {
+        box(20, map(perlin.get(xoff, yoff), 0, 1, 0, 200), 20);
       }
       pop();
+      counter += 1;
       yoff += scl_off;
     }
     xoff += scl_off;
